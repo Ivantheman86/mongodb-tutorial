773e6e76cd1335e2dc6e13f18b54bd4d06dac302
IvanTheMan86 <ivan.garrahan@gmail.com>
2019-08-13 16:21:05 +0100

    Removed edit of mongo_setup.py

--- src/starter_code_snake_bnb/src/data/mongo_setup.py
+++ src/starter_code_snake_bnb/src/data/mongo_setup.py
@@ -2,17 +2,10 @@ import mongoengine
 
 alias_core = 'core'
 db = 'snake_db'
-connection_string = u'mongodb://SoundsSelect:beatsbyer@testcluster-9feub.azure.mongodb.net/test?retryWrites=true&w=majority'
 
-
-# connection string:
-# mongodb+srv://SoundsSelect:<password>@testcluster-9feub.azure.mongodb.net/test?retryWrites=true&w=majority
 
 def global_setup():
     mongoengine.register_connection(
         alias=alias_core,
         name=db,
-        username='SoundsSelect',
-        password='beatsbyer',
-        host=connection_string,
     )
